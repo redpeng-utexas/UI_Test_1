a43b51f1a59f8f3e719af04bd5bfb53e63575f67
redpeng-utexas <redpeng@utexas.edu>
2026-02-01 15:12:42 -0600

    CSS Video Finished

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
+let canvas;
+let cursor;
+
 function setup() {
-  createCanvas(window.innerWidth, 400); //creates a square canvas, dimensions 400x400
+  canvas = createCanvas(window.innerWidth, window.innerHeight); //creates a square canvas, dimensions 400x400
+  canvas.position(0,0);
+  canvas.style('z-index', '-1')
   background(220);
+  cursor = loadImage('/Images/2051 Peugeot 3011.png');
 }
 
 function draw() {
   //set bg color to grey
   background(220);
-  circle (min(max(mouseX, 50), width-50), min(max(mouseY, 50), height-50), width*0.02)
+  image(cursor, min(max(mouseX, 50), width-50), min(max(mouseY-20, 50), height-50), width*0.05, height*0.035);
 }
