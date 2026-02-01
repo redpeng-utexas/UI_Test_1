8d9a44cbe1b19ed824f4ba281026881aaa8b7ad4
redpeng-utexas <redpeng@utexas.edu>
2026-02-01 14:35:15 -0600

    End of HTML Video

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(window.innerWidth, 400); //creates a square canvas, dimensions 400x400
+  background(220);
 }
 
 function draw() {
+  //set bg color to grey
   background(220);
-  
+  circle (mouseX, mouseY, width*0.02)
 }
